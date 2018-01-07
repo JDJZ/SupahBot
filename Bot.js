da935cca2ded4d837cf504f6b672ca1e190bcc42
JDJZ <JDJZ@users.noreply.github.com>
2018-01-06 17:47:07 -0800

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -139,8 +139,9 @@ function roll(content, message) {
 }
 
 function devList(content, message) {
-  message.reply(Helper.wrap('Made by the esteemed <@135821957844172800>');
+  message.reply(Helper.wrap('Made by the esteemed <@135821957844172800>'));
     }
+  
 }
 
 function isBotCommand(message) {
